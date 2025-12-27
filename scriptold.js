72f67d0c0743a238cc2094ca65615cf789809996
ChandanM1412 <chandanm14122002@gmail.com>
2025-12-27 16:37:00 +0530

    Your commit message

---
+++ scriptold.js
@@ -0,0 +1,280 @@
+var audio = new Audio('assets/sentmessage.mp3');
+
+var contactString = "<div class='social'>\
+  <!--\ <a target='_blank' href='tel:+917259956267'>\
+    <div class='socialItem' id='call'>\
+      <img class='socialItemI' src='images/phone.svg'/>\
+      <label class='number'>+91 7259956267</label>\
+    </div>\
+  </a>\
+  -->\
+  <!--\
+  <a target='_blank' href='https://wa.me/917259956267'>\
+    <div class='socialItem'>\
+      <img class='socialItemI' src='images/whatsapp.svg' alt=''>\
+    </div>\
+  </a>\
+  -->\
+  <a href='mailto:chanduchandan2019@gmail.com'>\
+    <div class='socialItem'>\
+      <img class='socialItemI' src='images/gmail.svg' alt=''>\
+    </div>\
+  </a>\
+  <a target='_blank' href='https://github.com/ChandanM1412'>\
+    <div class='socialItem'>\
+      <img class='socialItemI' src='images/github.svg' alt=''>\
+    </div>\
+  </a>\
+  <a target='_blank' href='https://instagram.com/chandan_aradhaya'>\
+    <div class='socialItem'>\
+      <img class='socialItemI' src='images/instagram.svg' alt=''>\
+    </div>\
+  </a>\
+  <a href='https://www.linkedin.com/in/chandan-m-701674213' target='_blank' rel='noopener noreferrer'>\
+    <div class='socialItem'>\
+      <img class='socialItemI' src='images/linkedin.svg' alt=''>\
+    </div>\
+  </a>\
+</div>";
+
+var resumeString = "<img src='images/resume.png' class='resumeThumbnail'>\
+<div class='downloadSpace'>\
+  <div class='pdfname'>\
+    <img src='images/pdf.png'>\
+    <label>Chandan M_Resume.pdf</label>\
+  </div>\
+  <a href='https://drive.google.com/drive/folders/1GeBSw7hPBuBUux1j-mB4TWz1bbYCNEXn?usp=drive_link' download='Chandan M_Resume.pdf'>\
+    <img class='download' src='images/downloadIcon.svg'>\
+  </a>\
+</div>";
+
+
+var addressString = "<div class='mapview'><iframe src='https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3883.375216071767!2d77.53015907494871!3d13.264476887078436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bb1df4fae79f5d9%3A0xb3ae3f842379f98d!2sChikka%20Tumkur!5e0!3m2!1sen!2sin!4v1683698994338!5m2!1sen!2sin' class='map'></iframe></div><label class='add'><address>Chikkatumkur<br>Veerapura Post<br>Doddaballapur Taluk -561203<br> Bengaluru Rural Dist<br> KA, India</address>";
+
+function startFunction() {
+    setLastSeen();
+    waitAndResponce("intro");
+}
+
+function setLastSeen() {
+    var date = new Date();
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "last seen today at " + date.getHours() + ":" + date.getMinutes()
+}
+
+
+function closeFullDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+function openFullScreenDP() {
+    var x = document.getElementById("fullScreenDP");
+    if (x.style.display === 'flex') {
+        x.style.display = 'none';
+    } else {
+        x.style.display = 'flex';
+    }
+}
+
+
+function isEnter(event) {
+    if (event.keyCode == 13) {
+        sendMsg();
+    }
+}
+
+function sendMsg() {
+    var input = document.getElementById("inputMSG");
+    var ti = input.value;
+    if (input.value == "") {
+        return;
+    }
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "sent");
+    greendiv.setAttribute("class", "green");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = input.value;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    setTimeout(function () { waitAndResponce(ti) }, 1000);
+    input.value = "";
+    playSound();
+}
+
+function waitAndResponce(inputText) {
+    var lastSeen = document.getElementById("lastseen");
+    lastSeen.innerText = "typing...";
+    switch (inputText.toLowerCase().trim()) {
+        case "intro":
+            setTimeout(() => {
+                sendTextMessage(
+                    "Hello there 👋🏻,<br><br>" +
+                    "My name is <span class='bold'><a class='alink'>Chandan M</a></span>.<br><br>" +
+                    "<span class='bold'>Programmer | Developer |<br>" +
+                    "Tech Enthusiast | Poetry Writer 👨🏻‍💻📚</span><br><br>" +
+                    "As an engineer, I am eager to learn new skills and explore trending domains in the field of computer science, which helps enhance my knowledge and abilities.<br><br>" +
+                    "This is a WhatsApp Portfolio Bot.<br>" +
+                    "Send <span class='bold'><a class='alink'>'Hi'</a></span> to know more."
+                );
+            }, 500);
+            break;
+
+        case "hi":
+//            sendTextMessage("<span class='sk'>Send coloured keyword to get what you want to know about me...<br>e.g<br>📍<span class='bold'>skills</span> - to know my skills<br>📍<span class='bold'>resume</span> - to get my resume<br>📍<span class='bold'>education</span> - to get my education details<br>📍<span class='bold'>address</span> - to get my address<br>📍<span class='bold'>contact</span> - to get ways to connect with me<br>📍<span class='bold'>projects</span> - to get details of my projects<br>📍<span class='bold'>about</span> - to know about this site<br>📍<span class='bold'>clear</span> - to clear conversation</span>");
+              sendTextMessage(
+                  "<span class='sk'>Send a coloured keyword to learn more about me...<br><br>" +
+                  "e.g.<br>" +
+                  "📍 <span class='bold'>skills</span> - to know my skills<br>" +
+                  "📍 <span class='bold'>education</span> - to get my education details<br>" +
+                  "📍 <span class='bold'>projects</span> - to get details of my projects<br>" +
+                  "📍 <span class='bold'>awards</span> - to view my awards, certifications, and patents<br>" +
+                  "📍 <span class='bold'>contact</span> - to get ways to connect with me<br>" +
+                  "📍 <span class='bold'>about</span> - to know about this site<br>" +
+                  "📍 <span class='bold'>clear</span> - to clear the conversation" +
+                  "</span>"
+              );
+              break;
+
+        case "resume":
+            sendTextMessage(resumeString);
+            break;
+        case "skills":
+            sendTextMessage(
+                "<span class='sk'>I can comfortably write code in the following languages:<br>" +
+                "<span class='bold'>Java<br>PHP<br>Python<br>CSS<br>HTML<br>SQL<br>JavaScript<br>Neo4j Graph Database<br>ELK (Elasticsearch, Logstash, Kibana)</span><br><br>" +
+                "I have worked with the following tools:<span class='bold'><br>" +
+                "Android Studio<br>VS Code<br>IntelliJ IDEA<br>Graph Sandbox<br>Neo4j Bloom / Browser<br>Findwise I3<br>Postman</span><br>" +
+                "</span>"
+            );
+            break;
+
+        case "education":
+            sendTextMessage(
+                "🌟 <span class='bold'>B.E. in Information Science & Engineering</span><br>" +
+                "Sai Vidya Institute of Technology, Bengaluru<br>" +
+                "Pass-out Year: 2024<br>" +
+                "CGPA: 8.8<br><br>" +
+
+                "🌟 <span class='bold'>12th / PUC (PCMB)</span><br>" +
+                "Vidyanidhi PU College, Tumkur<br>" +
+                "Pass-out Year: 2020<br>" +
+                "Percentage: 88%<br><br>" +
+
+                "🌟 <span class='bold'>10th / SSLC</span><br>" +
+                "Nalanda High School, Doddaballapur<br>" +
+                "Pass-out Year: 2018<br>" +
+                "Percentage: 90.4%"
+            );
+            break;
+
+        case "address":
+            sendTextMessage(addressString);
+            break;
+
+        case "clear":
+            clearChat();
+            break;
+
+        case "about":
+            sendTextMessage(
+                "🛠️💻 This WhatsApp-themed portfolio chatbot is built using HTML, CSS, and JavaScript.<br><br>" +
+                "👨🏻‍💻 Designed and developed by " +
+                "<a class='alink' target='_blank' href='https://instagram.com/chandan_aradhaya/'>" +
+                "<span class='bold'>Chandan M</span></a>"
+            );
+            break;
+
+        case "contact":
+            sendTextMessage("You can contact me using the options below:<br>" + contactString);
+            break;
+
+        case "projects":
+            sendTextMessage(
+                "Want to check out my projects? Visit my GitHub profile using the link below:<br><br>" +
+                "<div class='social'><a target='_blank' href='https://github.com/ChandanM1412'>" +
+                "<div class='socialItem'><img class='socialItemI' src='images/github.svg' alt=''></div>" +
+                "</a></div>"
+            );
+            break;
+
+        case "awards":
+            sendTextMessage(
+                "Do you want to check my awards, certifications and patents? Then visit them on my LinkedIn profile. Here is the link:<br><br>" +
+                "<div class='social'><a target='_blank' href='https://www.linkedin.com/in/chandan-m-701674213'>" +
+                "<div class='socialItem'><img class='socialItemI' src='images/linkedin.svg' alt=''></div>" +
+                "</a></div>"
+            );
+            break;
+        default:
+            setTimeout(() => {
+                sendTextMessage("Hey I couldn't catch you...😢<br>Send 'Hi' to know more about me.");
+            }, 500);
+            break;
+    }
+}
+
+function clearChat() {
+    document.getElementById("listUL").innerHTML = "";
+    waitAndResponce('intro');
+}
+
+function sendTextMessage(textToSend) {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.setAttribute("id", "sentlabel");
+    dateLabel.id = "sentlabel";
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    greendiv.innerHTML = textToSend;
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function sendResponse() {
+    setTimeout(setLastSeen, 1000);
+    var date = new Date();
+    var myLI = document.createElement("li");
+    var myDiv = document.createElement("div");
+    var greendiv = document.createElement("div");
+    var dateLabel = document.createElement("label");
+    dateLabel.innerText = date.getHours() + ":" + date.getMinutes();
+    myDiv.setAttribute("class", "received");
+    greendiv.setAttribute("class", "grey");
+    dateLabel.setAttribute("class", "dateLabel");
+    greendiv.innerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. ";
+    myDiv.appendChild(greendiv);
+    myLI.appendChild(myDiv);
+    greendiv.appendChild(dateLabel);
+    document.getElementById("listUL").appendChild(myLI);
+    var s = document.getElementById("chatting");
+    s.scrollTop = s.scrollHeight;
+    playSound();
+}
+
+function playSound() {
+    audio.play();
+}
